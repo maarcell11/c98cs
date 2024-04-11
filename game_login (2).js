b50616f310d61d2efe040fa2a6cb93131b5de616
maarcell11 <166670394+maarcell11@users.noreply.github.com>
2024-04-10 21:32:07 -0300

    Add files via upload

---
+++ game_login (2).js
@@ -0,0 +1,12 @@
+// Crie a função addUser()
+function send(){
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number) * parseInt (number2);
+}
+  // Obtenha o valor do usuário através das ids player1_name_input e player2_name_input
+
+  // Armazene esses valores localmente
+
+  // Atribua "game_page.html" para window.location
+
